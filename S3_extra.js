793fd5d45729a53da1565d2ff297afda25e39eab
Source Academy User <kurtyjlee>
2022-08-23 17:53:30 +0800

    Changes made from Source Academy

--- S3_extra.js
+++ S3_extra.js
@@ -1,19 +1,15 @@
-import {show, ribbon, square, heart, circle, beside, stack, blank, beside_frac, stack_frac} from "rune";
-
-// Type your program in here!
-function moony_1(bottom_right) {
-    return stack(beside(circle, blank), beside(square, bottom_right));
+//1. recursive as there is deferred operations.its linear in both space and time
+function fast_expt(b, n) {
+    return n % 2 === 0 
+        ? fast_expt(b ** 2, n / 2) 
+        : fast_expt(b , n - 1);
 }
 
-// Type your program in here!
-function moony_2(n) {
-    return n === 0
-        ? circle
-        : moony_1(moony_2(n - 1));
-}
 
-function moony(){
-    return 0;
+function fast_expt(n) {
+    return n === 1
+        ? math_pow(3, 1)
+        : n % 2 === 0
+            ? math_pow(fast_expt(n / 2), 2)
+            : 3 * fast_expt(n - 1);
 }
-
-show(moony_2(5));
