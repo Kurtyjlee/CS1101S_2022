50e315ef6d79a9a8b1a6bcfb92229d2acc4f484e
Source Academy User <kurtyjlee>
2022-08-23 17:53:40 +0800

    Changes made from Source Academy

--- S3_extra.js
+++ S3_extra.js
@@ -1,15 +1,17 @@
-//1. recursive as there is deferred operations.its linear in both space and time
-function fast_expt(b, n) {
-    return n % 2 === 0 
-        ? fast_expt(b ** 2, n / 2) 
-        : fast_expt(b , n - 1);
-}
+
+// Answers
+// //1. recursive as there is deferred operations.its linear in both space and time
+// function fast_expt(b, n) {
+//     return n % 2 === 0 
+//         ? fast_expt(b ** 2, n / 2) 
+//         : fast_expt(b , n - 1);
+// }
 
 
-function fast_expt(n) {
-    return n === 1
-        ? math_pow(3, 1)
-        : n % 2 === 0
-            ? math_pow(fast_expt(n / 2), 2)
-            : 3 * fast_expt(n - 1);
-}
+// function fast_expt(n) {
+//     return n === 1
+//         ? math_pow(3, 1)
+//         : n % 2 === 0
+//             ? math_pow(fast_expt(n / 2), 2)
+//             : 3 * fast_expt(n - 1);
+// }
