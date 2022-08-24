9c695b118443481316e2a4f3003a45970dcb2c5b
Source Academy User <kurtyjlee>
2022-08-25 01:33:23 +0800

    Changes made from Source Academy

--- R2_HOF_and_scope.js
+++ R2_HOF_and_scope.js
@@ -1,3 +1,4 @@
+// qn 2
 function my_sum(n) {
     return n === 0
         ? n
@@ -5,3 +6,24 @@ function my_sum(n) {
 }
 
 my_sum(2);
+
+// qn 4
+function term(n) {
+    return n * (n + 1);
+}
+
+function next(n) {
+    return n + 1
+}
+\
+
+//qn 5
+function sum(n) {
+    return sum_iter();
+}
+
+function sum_iter(start, end, result) {
+    return start > end
+        ? 0
+        : return sum_iter(start + 1, end, result + result + 1);
+}
