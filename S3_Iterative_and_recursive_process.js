3f9be142b59dbe53080a40f868ad61e705830b3e
kurtyjlee <No public email provided>
2022-08-24 10:37:57 +0800

    Changes made from Source Academy

--- S3_Iterative_and_recursive_process.js
+++ S3_Iterative_and_recursive_process.js
@@ -1,19 +1,34 @@
-import {show, ribbon, square, heart, circle, beside, stack, blank, beside_frac, stack_frac} from "rune";
+import {heart, square, blank, show, beside, stack, circle, ribbon
+    , stack_frac} from "rune";
 
-// Type your program in here!
 function moony_1(bottom_right) {
-    return stack(beside(circle, blank), beside(square, bottom_right));
+    return stack(beside(circle, blank),
+        beside(square, bottom_right));
 }
 
-// Type your program in here!
+// show(moony_1(ribbon));
+
 function moony_2(n) {
-    return n === 0
+    return n === 1
         ? circle
         : moony_1(moony_2(n - 1));
 }
 
-function moony(){
-    return 0;
+// show(moony_2(5));
+
+function moony_3(n){
+    return n === 1 
+           ? circle 
+           : beside_frac(1/n,stack_frac(1/n,circle, square),stack_frac(1/n,blank, moony_3(n-1))); 
 }
 
-show(moony_2(5));
+
+/*
+function moony(n){
+    return  n === 1
+            ? circle
+            : beside_frac(1/n,stack_frac(1/n,circle,square),stack_frac(1/n,blank,moony(n-1)));
+}
+*/
+
+//recur to iter, use xtra parameter to store the data
