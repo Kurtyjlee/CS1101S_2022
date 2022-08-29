ede5676cceb9b9db986aaa9676c5e54503630143
Source Academy User <kurtyjlee>
2022-08-29 21:54:19 +0800

    Changes made from Source Academy

--- S4_tree_recursion.js
+++ S4_tree_recursion.js
@@ -1,8 +1,11 @@
 function pascal(row, position) {
+    // Case 1: Point out of pascal triangle
     return row === 0 && position > row || row === 0 && position < 0
         ? 0
+        // Case 2: Top row
         : row === 0
         ? 1
+        // Recursively move up the pyramid
         : pascal(row - 1, position - 1) 
         + pascal(row - 1, position);
 }
