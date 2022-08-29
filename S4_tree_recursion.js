6bc2d72bb223f6c61f60b779ed46c2fea44b340b
Source Academy User <kurtyjlee>
2022-08-29 22:10:48 +0800

    Changes made from Source Academy

--- S4_tree_recursion.js
+++ S4_tree_recursion.js
@@ -1,3 +1,4 @@
+// Recursively find a point in a pascal triangle
 function pascal(row, position) {
     // Case 1: Point out of pascal triangle
     return row === 0 && position > row || row === 0 && position < 0
@@ -10,4 +11,16 @@ function pascal(row, position) {
         + pascal(row - 1, position);
 }
 
-display(pascal(4, 2));
+// Iterative process
+function pascal_2(row, position) {
+    return pascal_iter(row, position, 0, 1, 0);
+}
+
+function pascal_iter(row, position, counter, result, prev_result) {
+    // Case 1: Point out of pascal triangle
+    return counter >= row
+        ? result
+        : pascal_iter(row, position, counter + 1, result + prev_result, result);
+}
+
+display(pascal_2(2, );
