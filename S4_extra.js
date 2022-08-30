9eff91b5ac47ee4033ad3b42bd6979fcedc30c57
Source Academy User <kurtyjlee>
2022-08-30 17:54:21 +0800

    Changes made from Source Academy

--- S4_extra.js
+++ S4_extra.js
@@ -14,20 +14,35 @@ thrice(math_sqrt)(256)
 compose(compose(math_sqrt, math_sqrt), math_sqrt)(256)
 math_sqrt(math_sqrt(math_sqrt(256)));
 
-3. 
+3 p1.
 const thrice = f => repeated(f, 3);
 
-thrice(thrice(f)(0)) 
-repeated(f, n)(x) 
-n = 3^3
+Explanation:mukjuceqw s A
+function repeated(f, n) { 
+    return n === 0
+      ? x => x
+      : compose(f, repeated(f, n - 1));
+}
+==> compose(f, (compose(f, compose(f, x => x))))
+==> f => x => f(f(f(x)))
 
-const f = x => x + 1
-thrice(thrice)(f)(0);
-thrice(compose(compose(f, f), f))(0)
+p2
+const adder = x => x + 1;
+thrice(thrice)(adder)(0);
 thrice(f(f(f(x))))(0)
-const g = f(f(f(x)))
-g(g(g(x)))(0)
-9
+27;
+
+Explanation:
+const adder = x => x + 1;
+thrice(thrice)(adder)(0);
+(f => x => f(f(f(x))))(thrice)(adder)(0);
+(x => thrice(thrice(thrice(x))))(adder)(0);
+(thrice(thrice(thrice(adder))))(0);
+(thrice(thrice(x => adder(adder(adder(x))))))(0);
+*** Let adder be a
+(thrice(thrice(x => a(a(a(x))))))(0)
+===> (thrice(x => a(a(a(x => a(a(a(x => a(a(a(x)))))))))))(0)
+===> 27
 
 4a.
 ((thrice(thrice))(add1))(6);
@@ -37,7 +52,20 @@ Thrice will take thrice, where it will executes that
 function 3 times. Thrice inherently executes 3 times,
 hence so 9 + 6 = 15
 
+Answer: ((thrice(thrice)(adder))(6);
+27 + 6 = 33;
+
 b.
 ((thrice(thrice))(x => x))(compose);
+
+Answer: compose
+
+c.
+((thrice(thrice))(square))(1);
+1;
+
+d.
+((thrice(thrice))(square))(2);
+(2 ^ 2) ^ 27;
 
 */
