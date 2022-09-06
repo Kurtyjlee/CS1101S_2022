97cae7e85effcf2987de2f73c92e116eea942d5e
kurtyjlee <No public email provided>
2022-09-06 17:16:53 +0800

    Data abstraction

---
+++ S5_in_class.js
@@ -0,0 +1,40 @@
+// Question 1
+function every_second(target_list) {
+    // When the even value is the last value
+    return is_null(tail(target_list))
+        ? null
+        // Last odd value of the list
+        : is_null(tail(tail(target_list)))
+            ? pair(head(tail(target_list)), null)
+            : pair(head(tail(target_list)), every_second(tail(tail(target_list))));
+}
+
+// draw_data(every_second(list(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)));
+
+// Question 2
+function every_first(target_list){
+    // When the odd value is the last value
+    return is_null(tail(target_list))
+        ? null
+        // Last even value of the list
+        : is_null(tail(tail(target_list)))
+            ? pair(head(target_list), null)
+            : pair(head(target_list), every_second(tail(target_list)));
+}
+
+// Get a list of the sum of even position values and sum of odd position values
+function sum(another_list){
+    const even_list = every_first(another_list);
+    const odd_list = every_second(another_list);
+    
+    // Use to find the sum of the list
+    function sum_iter(target_list, sum) {
+        // Check if at the end of the list
+        return is_null(target_list)
+            ? sum
+            : sum_iter(tail(target_list), sum + head(target_list));
+    }
+    return list(sum_iter(even_list, 0), sum_iter(odd_list, 0));
+}
+
+draw_data(sum(list(1, 2, 3, 4, 5, 6)));
