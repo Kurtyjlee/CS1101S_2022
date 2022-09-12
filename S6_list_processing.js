9859ae4501f569c87a6918254c1e624d6aafbea5
Source Academy User <kurtyjlee>
2022-09-12 10:53:11 +0800

    Changes made from Source Academy

--- S6_list_processing.js
+++ S6_list_processing.js
@@ -1,1 +1,22 @@
+// function my_map(f, xs) {
+//     // this should be a one-liner
+//     return is_null(xs)
+//         ? null
+//         : pair(f(head(xs)), my_map(f, tail(xs)));
+// }
 
+function accumulate(f, initial, xs) {
+    return is_null(xs) 
+        ? initial
+        : f(head(xs), accumulate(f, initial, tail(xs)));
+}
+
+function my_map(f, xs) {
+    // this should be a one-liner
+    return accumulate(
+            (x, y) => pair(f(x), my_map(f, tail(xs))),
+            null,
+            xs);
+}
+
+my_map(x => x + 1, list(1, 2, 3));
