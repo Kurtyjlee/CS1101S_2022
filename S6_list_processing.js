0a050b77307ef99e096a033b8efa7cd51918de39
Source Academy User <kurtyjlee>
2022-09-12 11:32:22 +0800

    Changes made from Source Academy

--- S6_list_processing.js
+++ S6_list_processing.js
@@ -4,6 +4,7 @@
 //         : f(head(xs), accumulate(f, initial, tail(xs)));
 // }
 
+// Question 1
 function my_map(f, xs) {
     // this should be a one-liner
     return accumulate(
@@ -12,4 +13,19 @@ function my_map(f, xs) {
             xs);
 }
 
-my_map(x => x + 1, list(1, 2, 3));
+// Test
+// my_map(x => x + 1, list(1, 2, 3));
+
+// Question 2
+const sample = list(1, 2, 3, 4);
+const duplicates = list(1, 2, 3, 4, 4, 3, 2, 1, 2);
+
+function remove_duplicates(lst) {
+    function remove_iter(lst, saved) {
+        return filter(
+                x => !is_null(member(x, saved))
+                    ? , lst);
+    }
+}
+
+remove_duplicates(duplicates);
