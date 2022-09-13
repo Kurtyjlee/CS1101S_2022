e18a05691ef49de01f351873723539a4fbe4a521
Source Academy User <kurtyjlee>
2022-09-13 14:14:12 +0800

    Changes made from Source Academy

--- S6_list_processing.js
+++ S6_list_processing.js
@@ -32,4 +32,27 @@ function remove_duplicates(lst) {
 // remove_duplicates(duplicates);
 
 // Question 3
+function makeup_amount(x, coins) {
+    if (x === 0) {
+        return list(null);
+    } else if (x < 0 || is_null(coins)) {
+        return null;
+    } else {
+        // Combinations that do not use the head coin.
+        const combi_A = pair(makeup_amount(x, tail(coins)), null);
+        
+        // Combinations that do not use the head coin 
+        // for the remaining amount.
+        const combi_B = null;
 
+        // Combinations that use the head coin.
+        const combi_C = makeup_amount(x - head_coin, tail(coins));
+
+        return append(combi_A, combi_C);
+    }
+}
+
+makeup_amount(22, list(1, 10, 5, 20, 1, 5, 1, 50));
+// Result: list(list(20, 1, 1), list(10, 5, 1, 5, 1), list(1, 20, 1),
+//              list(1, 20, 1), list(1, 10, 5, 5, 1), 
+//              list(1, 10, 5, 1, 5))
