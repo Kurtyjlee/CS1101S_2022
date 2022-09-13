b5eb570d2355c7cea38f12a2e87d094881c9d98c
Source Academy User <kurtyjlee>
2022-09-13 17:16:25 +0800

    Changes made from Source Academy

--- S6_list_processing.js
+++ S6_list_processing.js
@@ -6,51 +6,51 @@
 
 // Important: Need to know map, accumulate and filter inside out
 
-// // Question 1
-// function my_map(f, xs) {
-//     // this should be a one-liner
-//     return accumulate(
-//             (x, y) => pair(f(x), my_map(f, tail(xs))),
-//             null,
-//             xs);
-// }
+// Question 1
+function my_map(f, xs) {
+    // this should be a one-liner
+    return accumulate(
+            (x, y) => pair(f(x), my_map(f, tail(xs))),
+            null,
+            xs);
+}
 
-// // Alternate question 1
-// // Just need y as accumulate recursively calls itself on the tail
-// function my_map_1(f, xs) {
-//     // this should be a one-liner
-//     return accumulate((x, y) => pair(f(x), y), null, xs);
-// }
+// Alternate question 1
+// Just need y as accumulate recursively calls itself on the tail
+function my_map_1(f, xs) {
+    // this should be a one-liner
+    return accumulate((x, y) => pair(f(x), y), null, xs);
+}
 
-// // Test
-// // my_map(x => x + 1, list(1, 2, 3));
+// Test
+// my_map(x => x + 1, list(1, 2, 3));
 
-// // Question 2
-// const duplicates = list(1, 2, 3, 4, 4, 3, 2, 1, 2);
+// Question 2
+const duplicates = list(1, 2, 3, 4, 4, 3, 2, 1, 2);
 
-// function remove_duplicates(xs) {
-//     return is_null(xs)
-//         ? null
-//         : pair(
-//             // Inputting the head
-//             head(xs),
-//             // Getting and inputting a list without item that just got inputted
-//             remove_duplicates(filter(x => x !== head(xs)), tail(xs)));
-// }
+function remove_duplicates(xs) {
+    return is_null(xs)
+        ? null
+        : pair(
+            // Inputting the head
+            head(xs),
+            // Getting and inputting a list without item that just got inputted
+            remove_duplicates(filter(x => x !== head(xs), tail(xs))));
+}
 
-// // Question 2 iterative
-// function remove_duplicates_1(xs) {
-//     function helper(xs, new_xs) {
-//         if (is_null(xs)) {
-//             return new_xs;
-//         } else {
-//             // Removing the element that has already been used
-//             const filtered = filter(x => !equal(x, head(xs)));
-//             return helper(filtered, pair(head(xs), new_xs));
-//         }
-//     }
-//     return helper(xs, null);
-// }
+// Question 2 iterative
+function remove_duplicates_1(xs) {
+    function helper(xs, new_xs) {
+        if (is_null(xs)) {
+            return new_xs;
+        } else {
+            // Removing the element that has already been used
+            const filtered = filter(x => !equal(x, head(xs)));
+            return helper(filtered, pair(head(xs), new_xs));
+        }
+    }
+    return helper(xs, null);
+}
 
 // remove_duplicates(duplicates);
 
