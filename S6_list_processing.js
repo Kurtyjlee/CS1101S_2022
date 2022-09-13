667c1a2f1d34652d1c4ccf15dfdf5e637c1f418f
Source Academy User <kurtyjlee>
2022-09-13 16:47:28 +0800

    Changes made from Source Academy

--- S6_list_processing.js
+++ S6_list_processing.js
@@ -4,6 +4,8 @@
 //         : f(head(xs), accumulate(f, initial, tail(xs)));
 // }
 
+// Important: Need to know map, accumulate and filter inside out
+
 // Question 1
 function my_map(f, xs) {
     // this should be a one-liner
@@ -13,25 +15,46 @@ function my_map(f, xs) {
             xs);
 }
 
+// Alternate question 1
+// Just need y as accumulate recursively calls itself on the tail
+function my_map_1(f, xs) {
+    // this should be a one-liner
+    return accumulate((x, y) => pair(f(x), y), null, xs);
+}
+
 // Test
 // my_map(x => x + 1, list(1, 2, 3));
 
 // Question 2
 const duplicates = list(1, 2, 3, 4, 4, 3, 2, 1, 2);
 
-function remove_duplicates(lst) {
-    return is_null(lst)
+function remove_duplicates(xs) {
+    return is_null(xs)
         ? null
         : pair(
             // Inputting the head
-            head(lst),
+            head(xs),
             // Getting and inputting a list without item that just got inputted
-            remove_duplicates(filter(x => !equal(x, head(lst)), tail(lst))));
+            remove_duplicates(filter(x => x !== head(xs)), tail(xs)));
+}
+
+// Question 2 iterative
+function remove_duplicates_1(xs) {
+    function helper(xs, new_xs) {
+        if (is_null(xs)) {
+            return new_xs;
+        } else {
+            // Removing the element that has already been used
+            const filtered = filter(x => !equal(x, head(xs)));
+            return helper(filtered, pair(head(xs), new_xs));
+        }
+    }
+    return helper(xs, null);
 }
 
 // remove_duplicates(duplicates);
 
-// Question 3
+// Question 3, not real attempt
 function makeup_amount(x, coins) {
     if (x === 0) {
         return list(null);
@@ -39,14 +62,14 @@ function makeup_amount(x, coins) {
         return null;
     } else {
         // Combinations that do not use the head coin.
-        const combi_A = pair(makeup_amount(x, tail(coins)), null);
+        const combi_A = makeup_amount(x, tail(coins));
         
         // Combinations that do not use the head coin z
         // for the remaining amount.
-        const combi_B = null;
+        const combi_B = makeup_amount(x - head(coins), tail(coins));
 
-        // Combinations that use the head coin.
-        const combi_C = makeup_amount(x - head_coin, tail(coins));
+        // Combinations that use the head coins
+        const combi_C = map(c => pair(head(coins), c), combi_B);
 
         return append(combi_A, combi_C);
     }
