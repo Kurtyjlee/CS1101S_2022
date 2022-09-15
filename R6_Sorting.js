b1e0e4d6d9bdc00249db87fd8172ed249dc24125
kurtyjlee <No public email provided>
2022-09-15 15:33:16 +0800

    Changes made from Source Academy

--- R6_Sorting.js
+++ R6_Sorting.js
@@ -1,66 +1,34 @@
-// // Accumulate function
-// function accumulate(f, initial, xs) {
-//     return is_null(xs) 
-//         ? initial
-//         : f(head(xs), accumulate(f, initial, tail(xs)));
-// }
-
-// // Question 1, flatten a given list of lists
-// // Only works for list or lists
-// function flatten_list(xs) {
-//     return accumulate(append, null, xs);
-// }
-
-// flatten_list(list(list(1, 2), list(3, 4, 5, 6), null, list(7, 8, 9)));
-
-// // Question 2
-// function tree_sum(xs) {
-//     return is_null(xs)
-//         ? 0
-//         : (is_list(head(xs))
-//             ? tree_sum(head(xs))
-//             : head(xs))
-//             + tree_sum(tail(xs));
-// }
-/* 
-- A function that handles trees can be recursively called as 
-a trees can either have trees or a data type as its element.
-- null is an empty list
-*/
-
-// const my_tree = list(1, list(2, list(3, 4), 5), list(6, 7)); 
-// tree_sum(my_tree);
-
-// // Question 3
-// function accumulate_tree(f, op, initial, tree) {
-//     return accumulate((x => is_list(head(x)) ? op : f)(tree), initial, tree);
-// }
-
-// const tree = list(1, list(2, list(3, 4), 5), list(6, 7));
-// accumulate_tree(x => list(x), append, null , tree);
-
-// Question 3 correct answer
-function accumulate_tree(f, op, initial, tree) {
-    // Takes in head(xs) and accumulate(...) as x and y, 
-    // checks if x is a list
-    return accumulate((x, y) => 
-                        is_list(x)
-                        // If x is a list, do the operation and go into tree
-                        ? op(accumulate_tree(f, op, initial, x), y)
-                        // Not a list, convert the element into intended value
-                        : op(f(x), y),
-                    initial, 
-                    tree);
+function insert_cmp(x, xs, cmp) {
+    return is_null(xs) 
+           ? list(x)
+           : cmp(x, head(xs)) 
+           ? pair(x, xs)
+           : pair(head(xs), insert_cmp(x, tail(xs), cmp));
 }
 
-const tree_1 = list(1, list(2, list(3, 4), 5), list(6, 7));
-accumulate_tree(x => x, (x, y) => x + y, 0, tree_1);
+function insertion_sort_cmp(xs, cmp) {
+    return is_null(xs) 
+           ? xs
+           : insert_cmp(head(xs), 
+                        insertion_sort_cmp(tail(xs), cmp),
+                        cmp);
+}
 
-/* Notes:
-- count_data_items, tree_sum is basically a special way to do
-accumulate
-- When it comes to count, need to convert the values of the elements
-of the tree into a usable value. In the case of the example, the 
-element is converted into 1, using x => 1.
-- Actual: If the element is a tree, call accumulate_tree.
-*/
+// Test
+const xs = list(6, 3, 8, 5, 1, 9, 6, 4, 2, 7);
+
+// (a)
+// insertion_sort_cmp(xs, ???);
+// Result: list(1, 2, 3, 4, 5, 6, 6, 7, 8, 9)
+
+// (b)
+// insertion_sort_cmp(xs, ???);
+// Result: list(9, 8, 7, 6, 6, 5, 4, 3, 2, 1)
+
+// (c)
+// insertion_sort_cmp(xs, ???);
+// Result: list(7, 2, 4, 6, 9, 1, 5, 8, 3, 6)
+
+// (d)
+// insertion_sort_cmp(xs, ???);
+// Result: list(2, 4, 6, 6, 8, 9, 7, 5, 3, 1)
