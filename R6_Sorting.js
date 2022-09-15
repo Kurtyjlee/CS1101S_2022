b991869876c6ca9746f2cd1e0e5e012ca6aefd12
Source Academy User <kurtyjlee>
2022-09-15 17:09:46 +0800

    Changes made from Source Academy

--- R6_Sorting.js
+++ R6_Sorting.js
@@ -31,7 +31,7 @@ const xs = list(6, 3, 8, 5, 1, 9, 6, 4, 2, 7);
 // Result: list(7, 2, 4, 6, 9, 1, 5, 8, 3, 6)
 
 // (d)
-// insertion_sort_cmp(xs, (x, y) => x % 2 === 0 && x < y);
+// insertion_sort_cmp(xs, (x, y) => x % 2 === 0 && x <= y);
 // Result: list(2, 4, 6, 6, 8, 9, 7, 5, 3, 1)
 
 // Question 2
