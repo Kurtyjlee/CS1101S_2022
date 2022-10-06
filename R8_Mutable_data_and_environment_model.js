1edad760e4925a4c69a4a3ab5a7bae4cb4e40bed
Source Academy User <kurtyjlee>
2022-10-06 15:28:58 +0800

    Changes made from Source Academy

---
+++ R8_Mutable_data_and_environment_model.js
@@ -0,0 +1,24 @@
+function make_withdraw(balance, password) {
+    let tries = 3;
+    function withdraw(amount, password_attempt) { 
+        if (tries === 0) {
+            return "Account disabled";
+        } else if (balance >= amount && password_attempt === password) {
+            balance = balance - amount;
+            return balance; 
+        } else if (balance < amount) {
+            return "Insufficient funds"; 
+        } else {
+            tries = tries - 1;
+            return "Wrong password";
+        }
+    }
+    return withdraw; 
+}
+
+const acc = make_withdraw(100, "my_password");
+acc(30, "his_passcode");
+acc(10, "sesame");
+acc(15, "canola");
+acc(25, "olive");
+acc(30, "my_password");
