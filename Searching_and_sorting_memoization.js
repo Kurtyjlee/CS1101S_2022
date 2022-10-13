ed09a6ef7eb227563c675aa0fbe72a846fdd6504
Source Academy User <kurtyjlee>
2022-10-13 17:27:41 +0800

    Changes made from Source Academy

--- Searching_and_sorting_memoization.js
+++ Searching_and_sorting_memoization.js
@@ -1,16 +1,46 @@
-// Type your program in here!
+// Question 1
 function make_optimized_search(A) {
     
     const B = [];
     
     // copying A into B
     for (let i = 0; i < array_length(A); i = i + 1) {
-        b[i] = A[i];
+        B[i] = A[i];
     }
     
-    // Dont need to implement the sort algorithm
-    merge_sort(B);
+    // // Dont need to implement the sort algorithm
+    // merge_sort(B);
     
-    // Dont need to implement the search algorithm
-    return x => binary_search(B, x);
+    // // Dont need to implement the search algorithm
+    // return x => binary_search(B, x);
 }
+
+// Question 2a
+function fib(n) {
+    const arr = [0, 1];
+    
+    // Main loop
+    for (let i = 2; i <= n; i = i + 1) {
+        arr[i] = arr[i - 1] + arr[i - 2];
+    }
+    
+    return arr[n];
+}
+
+// Question 2b
+function fib_2(n) {
+    let first = 0;
+    let second = 1;
+    let temp = 0;
+    
+    for (let i = 2; i <= n; i = i + 1) {
+        temp = first;
+        first = second;
+        second = second + temp;
+    }
+    
+    return second;
+}
+
+// Test
+fib(8);
