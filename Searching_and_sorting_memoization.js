e5be156e6814b9d03069a5fadd9497eab9648af3
kurtyjlee <No public email provided>
2022-10-13 17:22:24 +0800

    Changes made from Source Academy

---
+++ Searching_and_sorting_memoization.js
@@ -0,0 +1,16 @@
+// Type your program in here!
+function make_optimized_search(A) {
+    
+    const B = [];
+    
+    // copying A into B
+    for (let i = 0; i < array_length(A); i = i + 1) {
+        b[i] = A[i];
+    }
+    
+    // Dont need to implement the sort algorithm
+    merge_sort(B);
+    
+    // Dont need to implement the search algorithm
+    return x => binary_search(B, x);
+}
