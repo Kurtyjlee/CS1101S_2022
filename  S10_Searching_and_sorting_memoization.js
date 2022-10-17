a795ca003089726cfc1256c0790338cfe6420474
Source Academy User <kurtyjlee>
2022-10-18 00:23:41 +0800

    Changes made from Source Academy

---  S10_Searching_and_sorting_memoization.js
+++  S10_Searching_and_sorting_memoization.js
@@ -1,1 +1,20 @@
+// Question 2b
+function bubblesort_list(L) {
+    let end = length(L) - 1;
+    
+    function sort(lst) {
+        if (is_null(lst) || is_null(tail(lst))) {
+            return lst;
+        } else if (head(lst) > head(tail(lst))) {
+            let temp = head(lst);
+            set_head(lst, head(tail(lst)));
+            set_head(tail(lst), temp);
+        }
+        return sort(tail(lst));
+    }
+    for (let i = 0; i < )
+}
 
+const LL = list(3, 5, 2, 4, 1);
+bubblesort_list(LL);
+LL;
